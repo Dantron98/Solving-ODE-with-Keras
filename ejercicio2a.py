cd4210cb18669ef45097f6330e21b10ad7e2c8ed
Dantron98 <alandtl38@gmail.com>
2022-10-21 11:12:37 -0500

    Ya hace una aproximación de la solución de a la ecuacion diferencial y la gráfica

--- ejercicio2a.py
+++ ejercicio2a.py
@@ -6,15 +6,12 @@ from keras.optimizers import RMSprop, Adam
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
-
 import numpy as np
-
+#Hoola mundo 3
 class ODEsolver(Sequential):
-
     loss_tracker = keras.metrics.Mean(name="loss")
 
     def train_step(self, data):
-
         batch_size = tf.shape(data)[0]
         x = tf.random.uniform((batch_size, 1), minval=-5, maxval=5)
 
@@ -27,7 +24,7 @@ class ODEsolver(Sequential):
             y_0 = self(x_0, training=True)
             eq = x * dy + y_pred - x ** 2 * keras.backend.cos(x)
             ic = y_0
-            loss = keras.losses.mean_squared_error(0., eq) + keras.losses.mean_squared_error(0., ic)
+            loss = keras.losses.mean_squared_error(0., eq) + 2*keras.losses.mean_squared_error(0., ic)
 
         grads = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
@@ -40,22 +37,27 @@ class ODEsolver(Sequential):
 
 
 model = ODEsolver()
+
 model.add(Dense(10, activation='tanh', input_shape=(1,)))
 model.add(Dense(1, activation='tanh'))
 model.add(Dense(1, activation='linear'))
-
 model.summary()
 
 model.compile(optimizer=RMSprop(), metrics=['loss'])
+tf.keras.layers.Dropout(.25, input_shape=(2,))
+x = tf.linspace(-5, 5, 1000)
+history = model.fit(x, epochs=1000, verbose=1)
 
-x = tf.linspace(-5, 5, 100)
-history = model.fit(x, epochs=500, verbose=1)
-x_testv = tf.linspace(-5, 5, 100)
+x_testv = tf.linspace(-5, 5, 1000)
 y = [((x*np.sin(x))+(2*np.cos(x))-((2/x)*np.sin(x))) for x in x_testv]
+
 a = model.predict(x_testv)
+plt.grid()
+plt.title('Solución encontrada por la red vs solución analitica')
 plt.plot(x_testv, a)
 plt.plot(x_testv, y)
 plt.show()
+model.save('red2.1.h5')
 exit()
 
 model.save('red2.h5')
